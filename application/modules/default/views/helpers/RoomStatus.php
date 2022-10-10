833a4eb368ccea55d53d9065a2b0af0e09c7efab
Jacek <kontakt@jk-design.pl>
2022-10-10 03:19:41 +0200

    Poprawki klienta cz. 2

--- application/modules/default/views/helpers/RoomStatus.php
+++ application/modules/default/views/helpers/RoomStatus.php
@@ -5,13 +5,13 @@ class Zend_View_Helper_RoomStatus extends Zend_View_Helper_Abstract {
 	public function roomStatus($numer){
 		switch ($numer) {
 			case '1':
-				return 'Dostępny';
+				return 'Dostępne';
 			case '2':
-				return 'Sprzedany';
+				return 'Sprzedane';
 			case '3':
 				return 'Rezerwacja';
 			case '4':
-                return 'Wynajęty';
+                return 'Wynajęte';
             case '5':
                 return 'Do wynajęcia';
 		}
